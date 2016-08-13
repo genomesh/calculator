9e1d5bc8b6be842568191ce73d33a1a7e87b9ed3
Si <si@zeebox.com>
2016-08-13 11:43:24 +0100

    Use heroku env based port

--- app.js
+++ app.js
@@ -4,12 +4,11 @@ var path = require('path');
 
 var app = express();
 
-// app.get('/service/games', function (req, res) {
-//    res.send(['game1', 'tim vs joe', 'slimebusters']);
-// });
+app.set('port', (process.env.PORT || 3000));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(3000, function () {
-  console.log('Running calculator web service on port 3000!');
+app.listen(app.get('port'), function () {
+  console.log('Running calculator web service on port', app.get('port'));
 });
+
